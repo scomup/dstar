d2756109b3ba7fcba05d8f975fd6f930b95e27c9
liu <scomup@gmail.com>
2016-12-19 01:04:26 +0900

    3

--- dstar.py
+++ dstar.py
@@ -61,11 +61,11 @@ class grid_map:
                     
 
 class dstar_planner(threading.Thread):
-    def __init__(self,g_map):
+    def __init__(self,grid_map):
         threading.Thread.__init__(self)
         self.open = []
-        self.drawer = drawer.map_drawer(g_map)
-        self.grid_map = g_map
+        self.drawer = drawer.map_drawer(grid_map)
+        self.grid_map = grid_map
     def insert(self,cell,h_new):
         if cell.tag == 'NEW':
             cell.k = h_new
@@ -84,21 +84,28 @@ class dstar_planner(threading.Thread):
             for j in range(-1,2):
                 if (cell.x + i < rows ) and (cell.x + i >= 0 ) and (cell.y + j < cols ) and (cell.y + j >= 0 ): 
                     if i == 0 and j == 0:
-                        break
-                    if self.grid_map.map_data[cell.x + i][cell.y + j].tag == 'CLOSE':
-                        break
-                    print cell.x,cell.y
+                        continue
+                    #if self.grid_map.map_data[cell.x + i][cell.y + j].tag == 'CLOSE':
+                    #    continue
                     tmp.append(self.grid_map.map_data[cell.x + i][cell.y + j])
         return tmp
-    def process_state(self):
+
+    def clear_close(self):
+        rows = len(self.grid_map.map_data)
+        cols = len(self.grid_map.map_data[0])
+        for i in range(rows):
+            for j in range(cols):
+                if self.grid_map.map_data[i][j].tag == 'CLOSE':
+                    self.grid_map.map_data[i][j].tag = 'NEW'
+
+    def process_state(self,end_cell):
         if len(self.open) == 0:
             return -1
         self.open.sort(key=operator.attrgetter('k'))
-        #sorted(self.open, key=lambda cell: cell.k)
         cell_c = self.open[0]
         del self.open[0]
         cell_c.tag = 'CLOSE'
-        if cell_c.stat == 'START':
+        if cell_c == end_cell:
             return -1
         if cell_c == None or cell_c.stat == 'START':
             return -1
@@ -116,31 +123,82 @@ class dstar_planner(threading.Thread):
                     self.insert(cell, cell_c.cost(cell) + cell_c.h)
                     
         return k_old
-    def run(self):
+    def init_plan(self):
         k = 0
         while k != -1:
-            #time.sleep(1)
-            raw_input('pause : press any key ...')
-            k = planner.process_state()
-            self.drawer.update_plot()
-            self.drawer.fig.canvas.draw()
-            print 'RUN'
+            #raw_input('Press any key to next step ...')
+            k = planner.process_state(self.grid_map.start)
+            #self.drawer.update_plot()
+    def modify_cost(self,cell,cost):
+        self.insert(cell, cost)
+    def prepare_repair(self,cell_c):
+        neighbor = self.neighbor(cell_c)
+        for cell in neighbor:
+            if (cell.k == INF and cell.stat != 'OBSTACLE') or (cell.k != INF and cell.stat == 'OBSTACLE'):
+                if(cell.tag == 'CLOSE'):
+                    self.insert(cell,INF)
+
+    def sensor(self,cell):
+            if (cell.k == INF and cell.stat != 'OBSTACLE') or (cell.k != INF and cell.stat == 'OBSTACLE'):
+                self.insert(cell,cell.h)
+                neighbor = self.neighbor(cell)
+                for c in neighbor:
+                    self.insert(c,c.h)
+                return True
+            else:
+                return False
+
+    def prepare_replan(self,cell_c):
+        while True:
+            kmin = self.process_state(grid_map.start)
+            if kmin == -1 or kmin >= cell_c.h:
+                break
+
+    def run(self):
+        print '============= init plan start ==============='
+        self.init_plan()
+        self.drawer.update_plot()
+        print '============= init plan end ================='
+        grid_map.map_data[3][3].stat = 'OBSTACLE'
+        #self.clear_close()
+        cell_c = self.grid_map.start
+        while cell_c != self.grid_map.goal:
+            next_cell = cell_c.back_point
+            if self.sensor(next_cell):
+                k = 0
+                while k != -1:
+                    self.process_state(cell_c)
+                    self.drawer.update_plot()
+                    raw_input()
+            cell_c = next_cell 
+
 
             
                 
 if __name__ == '__main__':
-    g_map = grid_map(6,7)
-    g_map.set_goal(0,6)
-    g_map.set_start(5,1)
-    planner = dstar_planner(g_map)
-    planner.insert(g_map.goal,0)
+    grid_map = grid_map(6,7)
+    grid_map.set_goal(0,6)
+    grid_map.set_start(5,1)
+    grid_map.map_data[0][1].stat = 'OBSTACLE'
+    grid_map.map_data[0][2].stat = 'OBSTACLE'
+    grid_map.map_data[1][1].stat = 'OBSTACLE'
+    grid_map.map_data[1][2].stat = 'OBSTACLE'
+    grid_map.map_data[2][1].stat = 'OBSTACLE'
+    grid_map.map_data[2][2].stat = 'OBSTACLE'
+    grid_map.map_data[3][1].stat = 'OBSTACLE'
+    grid_map.map_data[3][2].stat = 'OBSTACLE'
+    grid_map.map_data[4][3].stat = 'OBSTACLE'
+
+
+    planner = dstar_planner(grid_map)
+    planner.insert(grid_map.goal,0)
     #planner.drawer.run()
     #test = showTrajectory()
     #test.start()
     #planner.drawer.start()    
     #self.fig.canvas.draw() 
     planner.start()
-    #drawer = drawer.map_drawer(g_map)
+    #drawer = drawer.map_drawer(grid_map)
     planner.drawer.run()
     #while k != -1:
     #    k = planner.process_state()
@@ -153,11 +211,11 @@ if __name__ == '__main__':
             
     
 
-#g_map = grid_map(6,7)
-#g_map.set_goal(0,6)
-#g_map.set_start(5,1)
-#planner = dstar_planner(g_map)
-#planner.insert(g_map.goal,0)
+#grid_map = grid_map(6,7)
+#grid_map.set_goal(0,6)
+#grid_map.set_start(5,1)
+#planner = dstar_planner(grid_map)
+#planner.insert(grid_map.goal,0)
 #planner.drawer.update_plot()
 #planner.drawer.start()
 #k = 0
@@ -166,7 +224,7 @@ if __name__ == '__main__':
 ##while k != -1 and i <step:
 ##    k = planner.process_state()
 #    #if i == step -1 or k == -1:
-#    #    g_map.update_plot(show_map)
+#    #    grid_map.update_plot(show_map)
 #     #   fig.canvas.draw()
 ##    i = i+1
 #
